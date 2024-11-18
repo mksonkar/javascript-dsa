21d5094fb4698e316e2e42ebca390734cd93070b
Mukesh Kumar <mkso13189@gmail.com>
2024-11-18 15:15:15 +0530

    Implement append function for linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -11,4 +11,17 @@ class LinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
 }
