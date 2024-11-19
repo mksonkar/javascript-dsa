2ddf3d0eecf5cdd7fdcae7c54e0a6318420cf66c
Mukesh Kumar <mkso13189@gmail.com>
2024-11-19 11:55:46 +0530

    Add prepend and print functions for linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -9,7 +9,7 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = 0;
+    this.size = 0;
   }
 
   append(value) {
@@ -24,4 +24,42 @@ class LinkedList {
     }
     this.size++;
   }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+  }
+
+  print() {
+    if (this.head === null) {
+      console.log("Empty list");
+      return;
+    }
+    let res = "";
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      res += currentNode.value + (currentNode.next && " -> ");
+      currentNode = currentNode.next;
+    }
+    console.log(res);
+  }
 }
+
+let myList = new LinkedList();
+myList.append(4);
+myList.append(5);
+myList.append(6);
+myList.append(7);
+myList.prepend(3);
+myList.prepend(2);
+myList.prepend(1);
+console.log(myList.size);
+myList.insert(myList.size, "A");
+myList.print();
